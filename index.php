810582d737ad664196e3b57dfb6626b887364825
gamercool57 <32177692+gamercool57@users.noreply.github.com>
2017-09-21 23:01:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Подключаем jquery -->
+<script src='http://code.jquery.com/jquery-1.7.1.js'></script>
+<!-- Подключаем VK-->
+<script src="http://vk.com/js/api/xd_connection.js?2" type="text/javascript"></script>
+<!-- Подключаем скрипт регулировки высоты-->
+<script src="vk_height.js" type="text/javascript"></script>
+<!-- Скрипт получения фоток друзей и непосредственно их вывод-->
+<script>
+// Функция получения друзей и вывода их на экран
+function doIt(){
+        /*
+        VK.api('метод', {параметр: "значение"}, callback функция{
+        // работаем с полученными данными
+        });
+        */
+
+	VK.api("friends.get", {fields: "photo_medium"}, function(data) { 
+		// Действия с полученными данными 
+		for(c=0;c<data.response.length;c++){
+			$("body").append("<img src='"+data.response[c].photo_medium+"'><br><br>");
+		}
+	});
+}
+$(document).ready(function(){
+	doIt();
+});
+</script>
+</head>
+<body style='text-align:center;padding-top:50px;' id='body'>
+</body>
+</html>
